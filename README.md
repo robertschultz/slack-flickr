9209003ae61a3c6d4270662b9846187ff3d0b14d
Robert Schultz <rschultz@ancestry.com>
2016-04-13 10:58:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Slack Flickr
+=====
+Simple Slack service that provides a random photo from Flickr.
+
+### Installation
+
+```
+$ git clone <url>
+cd <app>
+npm Installation
+```
+
+### Usage
+
+```
+$ API_KEY=<apiKey> TERMS="cute baby goats" npm start
+```
+
+### Parameters
+
+| Key  |  Required | Description  |
+|---|---|---|
+| API_KEY  | Yes  |  Your Flickr API key |
+| TERMS  | No  | Search terms to run against the Flickr API   |
+| MAX_RESULTS  | No  | How many results to pick a random image from  |
---
+++ app.js.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const debug = require('debug');
+const Flickr = require('node-flickr');
+
+if (!process.env.API_KEY) {
+  throw new Error('Flickr API key not provided');
+}
+
+const flickr = new Flickr({
+  api_key: process.env.API_KEY
+});
+
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
+const results = process.env.MAX_RESULTS || 50;
+const terms = process.env.TERMS || 'cute baby goats';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post('/', (req, res) => {
+  flickr.get('photos.search', {'per_page': results, 'text': terms}, function(err, result){
+    if (err) throw new Error(err);
+    var photo = result.photos.photo[Math.floor(Math.random() * result.photos.photo.length) ];
+
+    flickr.get('photos.getSizes', {'photo_id': photo.id}, function(err, result){
+      if (err) throw new Error(err);
+      res.json({
+        response_type: 'in_channel',
+        text: result.sizes.size[5].source
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
+  debug('Slack flickr service listening on port ' + port);
+});
+
+
+//017b9cc756a0d8d46e842e7a76ca238a
